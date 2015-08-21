e53fb2724c0854bb2c0920afe39fd36f2510b614
Ben Sheppard <benmsheppard@gmard@gmail.com>
2015-08-21 17:48:37 -0500

    first commit

---
+++ Tournament/tournament.py
@@ -0,0 +1,124 @@
+ #!/usr/bin/env python
+# 
+# tournament.py -- implementation of a Swiss-system tournament
+#
+
+import psycopg2
+
+
+def connect():
+    """Connect to the PostgreSQL database.  Returns a database connection."""
+    return psycopg2.connect("dbname=tournament")
+
+def deleteMatches():
+    """Remove all the match records from the database."""
+    db = connect()
+    cursor = db.cursor()
+    cursor.execute("DELETE FROM Matches")
+    db.commit()
+    cursor.close()
+    db.close();
+def deletePlayers():
+    """Remove all the player records from the database."""
+    db = connect()
+    cursor = db.cursor()
+    cursor.execute("DELETE FROM Players")
+    db.commit()
+    cursor.close()
+    db.close()
+
+def countPlayers():
+    """Returns the number of players currently registered."""
+    db = connect()
+    cursor = db.cursor()
+    cursor.execute("SELECT count(*) AS num FROM Players")
+    numPlayers = cursor.fetchone()[0]
+    cursor.close()
+    db.close()
+    return numPlayers
+def registerPlayer(name):
+    """Adds a player to the tournament database.
+
+    The database assigns a unique serial id number for the player.  (This
+    should be handled by your SQL database schema, not in your Python code.)
+  
+    Args:
+      name: the player's full name (need not be unique).
+    """
+
+    db = connect()
+    cursor = db.cursor()
+    cursor.execute("INSERT INTO Players(name) VALUES (%s)", (name,))
+    db.commit()
+    db.close()
+
+def playerStandings():
+    """Returns a list of the players and their win records, sorted by wins.
+
+    The first entry in the list should be the player in first place, or a player
+    tied for first place if there is currently a tie.
+
+    Returns:
+      A list of tuples, each of which contains (id, name, wins, matches):
+        id: the player's unique id (assigned by the database)
+        name: the player's full name (as registered)
+        wins: the number of matches the player has won
+        matches: the number of matches the player has played
+    """
+    db = connect()
+    cursor = db.cursor()
+    cursor.execute("SELECT id, name, numWins, numMatches FROM playerStandings ORDER BY numWins DESC")
+    numRows = cursor.fetchall()  
+    db.commit()
+    db.close()
+    return numRows
+def reportMatch(winner, loser):
+    """Records the outcome of a single match between two players.
+
+    Args:
+      winner:  the id number of the player who won
+      loser:  the id number of the player who lost
+    """
+    db = connect()
+    cursor = db.cursor()
+    cursor.execute("INSERT INTO Matches (p1, p2, wins) values(%s, %s,1)", (winner, loser))
+    cursor.execute("INSERT INTO Matches (p1, p2, wins) values(%s, %s,0)", (loser, winner))
+    db.commit()
+    db.close()
+
+ 
+ 
+def swissPairings():
+    """Returns a list of pairs of players for the next round of a match.
+  
+    Assuming that there are an even number of players registered, each player
+    appears exactly once in the pairings.  Each player is paired with another
+    player with an equal or nearly-equal win record, that is, a player adjacent
+    to him or her in the standings.
+  
+    Returns:
+      A list of tuples, each of which contains (id1, name1, id2, name2)
+        id1: the first player's unique id
+        name1: the first player's name
+        id2: the second player's unique id
+        name2: the second player's name
+    """
+    db = connect()
+    cursor = db.cursor()
+    cursor.execute("SELECT id, name, numWins, numMatches FROM playerStandings ORDER BY numWins DESC;")
+    playerTable = cursor.fetchall()
+    db.close()
+    i = 0
+    matches = []
+    while i< len(playerTable):
+        player1_id = playerTable[i][0]
+        player1_name = playerTable[i][1]
+        player2_id = playerTable[i+1][0]
+        player2_name = playerTable[i+1][1]
+        matches.append((player1_id, player1_name, player2_id, player2_name))
+        i = i + 2
+
+    return matches
+    
+
+
